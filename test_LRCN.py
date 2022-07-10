f5971e33afc5d284b3e416a651222498d7493b8b
Jihoon Kim <jihoonkim888@gmail.com>
2022-07-10 15:34:43 +0100

    Add true batch functionality to LRCN

--- test_LRCN.py
+++ test_LRCN.py
@@ -5,19 +5,21 @@ from torchinfo import summary
 input_dim = 64
 output_dim = 128
 c = 5
-input_shape = (1, 1, input_dim, input_dim, input_dim)
+batch_size = 2
+
+input_shape = (batch_size, 1, input_dim, input_dim, input_dim)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('device:', device)
 print('input shape:', input_shape)
 
 net = LRCN(input_dim=input_dim, kernel_size=3,
-           output_dim=output_dim, hidden_size=1000)
+           output_dim=output_dim, hidden_size=1000, batch_size=batch_size)
 net.apply(weights_init)
-print("\n\nGenerator summary\n\n")
-summary(net, input_shape)
+# print("\n\nGenerator summary\n\n")
+# summary(net, input_shape)
 
-input = torch.randn((5, 1, input_dim, input_dim, input_dim))
+input = torch.randn((batch_size, 1, input_dim, input_dim, input_dim))
 with torch.no_grad():
     output = net(input)
-    print(output.shape)
-    # print(output)
+print(output.shape)
+# print(output)

94d609fd941d37c72627afe04795158d41c2ab26
Jihoon Kim <jihoonkim888@gmail.com>
2022-07-18 23:03:29 +0100

    Improve code readability

--- upscale.py
+++ upscale.py
@@ -13,20 +13,21 @@ import os
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-id', '--input_dim', type=int, required=False)
-parser.add_argument('-od', '--output_dim', type=int, required=False)
 parser.add_argument('-dp', '--data_path', type=str,
                     required=True, help='Path to .npy file')
 parser.add_argument('-wp', '--weight_path', type=str, required=True)
-parser.add_argument('-e', '--epoch', type=int, required=False)
+parser.add_argument('-sp', '--save_path', type=str, required=True)
+parser.add_argument('-we', '--weight_epoch', type=int, required=False)
 args = parser.parse_args()
 
 # argparse
 data_path = args.data_path
 weights_path = args.weight_path
-input_dim = args.input_dim if args.input_dim else 64
-output_dim = args.output_dim if args.output_dim else 256
-epoch = args.epoch if args.epoch else None
+save_path = args.save_path
+input_dim = 64
+output_dim = 256
+weight_epoch = args.weight_epoch if args.weight_epoch else None
+b_size = 50
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('device:', device)
@@ -37,18 +38,25 @@ def init_upscaler(input_dim, output_dim):
     return net
 
 
+def find_weight_epoch():
+    if weight_epoch is not None:
+        epoch = weight_epoch
+    else:
+        weights_available = [i.strip('.pth').split('_')[-1]
+                             for i in os.listdir(weights_path)]
+        weights_available.sort()
+        epoch = weights_available[-1]
+    return epoch
+
+
 if __name__ == '__main__':
     net = init_upscaler(
         input_dim=input_dim, output_dim=output_dim)
     net = net.to(device)
 
-    if epoch is None:
-        weights_available = [i.strip('.pth').split('_')[-1]
-                             for i in os.listdir(weights_path)]
-        weights_available.sort()
-        epoch = weights_available[-1]
+    weight_epoch = find_weight_epoch()
 
-    net_filename = f'net_r{input_dim}_r{output_dim}_{epoch}.pth'
+    net_filename = f'net_r{input_dim}_r{output_dim}_{weight_epoch}.pth'
     print('weights to load:', net_filename)
     net.load_state_dict(torch.load(
         os.path.join(weights_path, net_filename)))
@@ -56,9 +64,14 @@ if __name__ == '__main__':
     with open(data_path, 'rb') as f:
         arr = np.load(f)
     input_tensors = torch.tensor(arr)
-    with torch.no_grad():
-        output = net(input_tensors.to(device))
-        output = output.cpu().numpy()
-    with open('upscaled.npy', 'wb') as f:
+    lst_samples = []
+    for t in torch.split(input_tensors, b_size):
+        with torch.no_grad():
+            samples = net(t.to(device)).cpu()
+            lst_samples.append(samples)
+    output = torch.cat(lst_samples)
+    output = output.numpy()
+    print('Output shape:', output.shape)
+    with open(save_path, 'wb') as f:
         np.save(f, output)
-    print('saved output as upscaled.npy')
+    print('Output saved at', save_path)

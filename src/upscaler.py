988b5e0ea47e6c020ac2833201947f86266da29f
Jihoon Kim <jihoonkim888@gmail.com>
2022-07-30 18:58:58 +0100

    Update to make 128 work

--- src/upscaler.py
+++ src/upscaler.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchinfo import summary
 
 
 class Upscaler(nn.Module):
-    def __init__(self, in_channels=1, input_dim=64, output_dim=256):
+    def __init__(self, in_channels=1, conv_channels=64, input_dim=64, output_dim=256):
         '''
         Replicating the generator structure of Yang et al. (2018), available at: https://github.com/Yang7879/3D-RecGAN-extended
         '''
@@ -14,15 +15,28 @@ class Upscaler(nn.Module):
         self.output_dim = output_dim
         self.scale = output_dim / input_dim
 
-        self.up_conv1_channels = 64
+        self.up_conv1_channels = conv_channels
         self.up_conv2_channels = in_channels
 
-        self.up_conv1 = nn.Sequential(
-            nn.ConvTranspose3d(in_channels=in_channels,
-                               out_channels=self.up_conv1_channels, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(),
-        )
-        if output_dim == 256:
+        if output_dim == 128:
+            self.up_conv1 = nn.Sequential(
+                nn.ConvTranspose3d(in_channels=self.in_channels,
+                                   out_channels=self.up_conv1_channels, kernel_size=4, stride=2, padding=1),
+                nn.ReLU(),
+            )
+
+            self.up_conv2 = nn.Sequential(
+                nn.ConvTranspose3d(in_channels=self.up_conv1_channels,
+                                   out_channels=in_channels, kernel_size=1, stride=1, padding=0),
+                nn.ReLU(),
+            )
+
+        elif output_dim == 256:
+            self.up_conv1 = nn.Sequential(
+                nn.ConvTranspose3d(in_channels=in_channels,
+                                   out_channels=self.up_conv1_channels, kernel_size=4, stride=2, padding=1),
+                nn.ReLU(),
+            )
             self.up_conv2 = nn.Sequential(
                 nn.ConvTranspose3d(in_channels=self.up_conv1_channels,
                                    out_channels=in_channels, kernel_size=4, stride=2, padding=1),
@@ -31,8 +45,7 @@ class Upscaler(nn.Module):
 
     def forward(self, x):
         x = self.up_conv1(x)
-        if self.output_dim == 256:
-            x = self.up_conv2(x)
+        x = self.up_conv2(x)
         return x
 
 
@@ -46,3 +59,14 @@ def BCELoss_w(output, target, weights=None, eps=1e-10):
         loss = torch.mul(target, torch.log(output)) + \
             torch.mul((1 - target), torch.log(1 - output))
     return torch.neg(torch.mean(loss))
+
+
+if __name__ == '__main__':
+    input_dim = 64
+    output_dim = 128
+    conv_channels = 64
+
+    input_size = (1, 1, input_dim, input_dim, input_dim)
+    ups = Upscaler(input_dim=input_dim, output_dim=output_dim,
+                   conv_channels=conv_channels)
+    summary(ups, input_size)
